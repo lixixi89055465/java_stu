a04e9b71053a32579155b83a36ee61e0ed20598c
nanji <1850094299@qq.com>
2020-03-31 23:02:35 +0800

    单一职责

--- DesignPattern/src/main/java/com/atguigu/principle/Singleresponsibility2.java
+++ DesignPattern/src/main/java/com/atguigu/principle/Singleresponsibility2.java
@@ -14,6 +14,12 @@ public class Singleresponsibility2 {
     }
 }
 
+/**
+ * 方案二
+ * 1。准守了单一职责原则
+ * 2。但是这样类的改动很大，即将类分解。同时修改客户端
+ * 3。改进，直接修改vehicle 类，改动的代码会比较少
+ */
 class RoadVehicle {
     public void run(String vehicle) {
         System.out.println(vehicle + "\t在公路上运行");
---
+++ DesignPattern/src/main/java/com/atguigu/principle/Singleresponsibility3.java
@@ -0,0 +1,38 @@
+package com.atguigu.principle;
+
+/**
+ * @author lixiang
+ * @date 2020-03-31-21:50
+ */
+public class Singleresponsibility3 {
+    public static void main(String[] args) {
+        Vehicle2 vehicle2 = new Vehicle2();
+        vehicle2.run("汽车");
+        vehicle2.runWater("轮船");
+        vehicle2.runAir("飞机");
+    }
+}
+
+
+//交通工具类
+
+/**
+ * 方式三的分析
+ * 1。这种修改方法没有对原来的类做大的修改，知识增加了方法。
+ * 2。这里虽然没有在类这个级别准守单一职责原则，但是方法级别上，仍然是遵守单一职责原则
+ */
+class Vehicle2 {
+    public void run(String vehicle) {
+        System.out.println(vehicle + "\t在公路上跑...");
+    }
+
+    public void runAir(String vehicle) {
+        System.out.println(vehicle + "\t在天空上运行...");
+    }
+
+    public void runWater(String vehicle) {
+        System.out.println(vehicle + "\t在水中行 ...");
+    }
+
+
+}
