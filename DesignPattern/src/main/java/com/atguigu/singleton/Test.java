16d18f284af0a52fa5ebe9f2cb26f152a899f044
nanji <1850094299@qq.com>
2020-04-02 23:12:02 +0800

    单例模式完结

---
+++ DesignPattern/src/main/java/com/atguigu/singleton/Test.java
@@ -0,0 +1,16 @@
+package com.atguigu.singleton;
+
+/**
+ * Runtime是一个经典的单例模式
+ * 
+ * @author lixiang
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Runtime.getRuntime();
+	}
+
+}
