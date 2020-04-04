3109185cc7bd6fb02a895bd08c79f1f98950efa7
nanji <1850094299@qq.com>
2020-04-04 18:39:19 +0800

    适配模式finish

--- DesignPattern/src/main/java/com/atguigu/adapter/interfaceadapter/AbsAdapter.java
+++ DesignPattern/src/main/java/com/atguigu/adapter/interfaceadapter/AbsAdapter.java
@@ -2,6 +2,30 @@ package com.atguigu.adapter.interfaceadapter;
 
 //在AbsAdapter 我们将Interface4的方法进行空实现 
 public abstract class AbsAdapter implements Interface4 {
+
+	@Override
+	public void m1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void m2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void m3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void m4() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 
---
+++ DesignPattern/src/main/java/com/atguigu/adapter/test/AdapterSpring.java
@@ -0,0 +1,9 @@
+package com.atguigu.adapter.test;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AdapterSpring {
+	public static void main(String[] args) {
+	}
+
+}
