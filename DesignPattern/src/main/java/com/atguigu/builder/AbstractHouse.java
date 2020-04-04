67d52bf08892452f18043f0fd737d3b5cda6c8ee
nanji <1850094299@qq.com>
2020-04-04 10:25:16 +0800

    建造者模式

---
+++ DesignPattern/src/main/java/com/atguigu/builder/AbstractHouse.java
@@ -0,0 +1,18 @@
+package com.atguigu.builder;
+
+public abstract class AbstractHouse {
+	// 打地基
+	public abstract void buildBasic();
+
+	// 砌墙
+	public abstract void buildWall();
+
+	// 封顶
+	public abstract void roofed();
+
+	public void build() {
+		buildBasic();
+		buildWall();
+		roofed();
+	}
+}
---
+++ DesignPattern/src/main/java/com/atguigu/builder/CommonHouse.java
@@ -0,0 +1,24 @@
+package com.atguigu.builder;
+
+public class CommonHouse extends AbstractHouse {
+
+	@Override
+	public void buildBasic() {
+		// TODO Auto-generated method stub
+		System.out.println("普通房子打地基 ");
+	}
+
+	@Override
+	public void buildWall() {
+		// TODO Auto-generated method stub
+		System.out.println("普通房子砌墙");
+
+	}
+
+	@Override
+	public void roofed() {
+		// TODO Auto-generated method stub
+		System.out.println("普通房子封顶");
+	}
+
+}
