8abcfa8df26efc1440f2d5635b1d601c178e7db6
nanji <1850094299@qq.com>
2020-04-04 14:53:28 +0800

    类适配器模式

---
+++ DesignPattern/src/main/java/com/atguigu/adapter/classadapter/IVoltage5V.java
@@ -0,0 +1,6 @@
+package com.atguigu.adapter.classadapter;
+
+public interface IVoltage5V {
+	public int output5V();
+
+}
---
+++ DesignPattern/src/main/java/com/atguigu/adapter/classadapter/Voltage220V.java
@@ -0,0 +1,12 @@
+package com.atguigu.adapter.classadapter;
+
+public class Voltage220V {
+	public int output220V() {
+		//输出220V的电压 
+		int src = 220;
+		System.out.println("电压=" + src + "伏特");
+		return src;
+	}
+	
+	
+}
