49f93e1437e974cafd26d0e5161c8a2ea876b813
lixiang <lixiang@qq.com>
2020-04-06 12:36:05 +0800

    模板模式_购置方法

---
+++ DesignPattern/src/main/java/com/atguigu/template/improve/PeanutSoyaMilk.java
@@ -0,0 +1,8 @@
+package com.atguigu.template.improve;
+
+public class PeanutSoyaMilk extends SoyaMilk{
+	@Override
+	void addCondiments() {
+		System.out.println("加入上号的花生 ！！");
+	}
+}
---
+++ DesignPattern/src/main/java/com/atguigu/template/improve/SoyaMilk.java
@@ -0,0 +1,38 @@
+package com.atguigu.template.improve;
+
+//豆浆抽象类  
+public abstract class SoyaMilk {
+	// 模板方法，make,模板方法可以做成final ,不让子类去覆盖
+	final void make() {
+		select();
+		if (customerWantCondiments()) {
+			addCondiments();
+			soak();
+			beat();
+		}
+
+	}
+
+	// 选材料
+	void select() {
+		System.out.println("第一步：选择好的新鲜黄豆 ");
+	}
+
+	// 添加不同的配料 ,抽象方法，子类具体实现
+	abstract void addCondiments();
+
+	// 浸泡
+	void soak() {
+		System.out.println("第三步:黄豆和配料开始浸泡 ,需要三个小时 ");
+	}
+
+	void beat() {
+		System.out.println("第四步：黄豆和配料，放到豆浆机去打碎 ");
+	}
+
+	// 钩子方法，决定是否需要添加配料
+	boolean customerWantCondiments() {
+		return true;
+	}
+
+}
