a1388057d519c0afb12e7a3da497ae9ea79e3997
nanji <1850094299@qq.com>
2020-04-08 22:45:33 +0800

    swagger_finish

--- swagger_stu/src/main/java/com/swagger/controller/UserController.java
+++ swagger_stu/src/main/java/com/swagger/controller/UserController.java
@@ -2,9 +2,9 @@ package com.swagger.controller;
 
 import com.swagger.pojo.User;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,5 +31,24 @@ public class UserController {
         return map;
     }
 
+    @ApiOperation(value = "获取单个用户", notes = "根据ID 查询某个用户")
+    @ApiImplicitParam(value = "用户的ID", paramType = "path", dataType = "int")
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") int id) {
+        return users.get(id);
+    }
 
+    @ApiOperation(value = "添加用户", notes = "根据传入的用户信息添加用户")
+    @ApiImplicitParam(value = "用户对象", paramType = "query")
+    @PostMapping("/user")
+    public Object add(@RequestBody User user) {
+        return users.add(user);
+    }
+
+    @ApiOperation(value = "删除用户",notes = "根据用户传入的用户ID删除对应的用户 ")
+    @ApiImplicitParam(value = "用户的ID",paramType = "path")
+    @DeleteMapping("/user/{id}")
+    public Object delete(@PathVariable("id")int id){
+        return users.remove(id);
+    }
 }
