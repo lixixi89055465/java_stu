873a741615c71d3c16633784aa614df027316c0c
nanji <1850094299@qq.com>
2020-05-19 08:58:34 +0800

    并发:信号量

--- interview_2019/src/main/java/com/atguigu/Interview/study/thread/SemaphoreDemo.java
+++ interview_2019/src/main/java/com/atguigu/Interview/study/thread/SemaphoreDemo.java
@@ -1,6 +1,7 @@
 package com.atguigu.Interview.study.thread;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author lixiang
@@ -8,8 +9,26 @@ import java.util.concurrent.Semaphore;
  */
 public class SemaphoreDemo {
     public static void main(String[] args) {
-        Semaphore semaphore = new Semaphore(3);
-        for (int i = 0; i < 6; i++) {
+        Semaphore semaphore = new Semaphore(3);//模拟3个停车位
+        for (int i = 0; i < 6; i++) { //模拟6部汽车
+            new Thread(() -> {
+                try {
+                    semaphore.acquire();
+                    //抢到车位
+                    System.out.println(Thread.currentThread().getName() + "\t 抢到车位");
+                    //暂停一会 线程
+                    try {
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                    //抢到车位
+                    semaphore.release();
+                    System.out.println(Thread.currentThread().getName() + "\t 停车3秒钟离开车位");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }, String.valueOf(i)).start();
 
         }
     }
