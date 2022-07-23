199c5b01318cb353bf13ca08c40a112c90662329
1850094299@qq.com <1850094299@qq.com>
2022-07-23 19:56:01 +0800

    java11

--- base/src/main/java/com/testjava11/Test02.java
+++ base/src/main/java/com/testjava11/Test02.java
@@ -6,6 +6,16 @@ public class Test02 {
     public static void main(String[] args) {
         String str="ABCD";
         System.out.println(str.repeat(3));
+        var str2="342\n343\n3242\n4323433333";
+        System.out.println(str2.lines());
+        System.out.println(str2.lines().count());
+        System.out.println();
+        str2=" A B C D ";
+        System.out.println(str2.strip());
+        System.out.println(str2.strip());
+        System.out.println(str2.stripLeading());
+        System.out.println(str2.stripTrailing());
+
 
     }
 }
