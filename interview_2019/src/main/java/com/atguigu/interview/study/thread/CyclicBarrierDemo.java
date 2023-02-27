471c379cfb97cc53110ff18182faaad9308499ae
lixixi89055465 <1850094299@qq.com>
2023-02-27 21:54:32 +0800

    guc

--- interview_2019/src/main/java/com/atguigu/interview/study/thread/CyclicBarrierDemo.java
+++ interview_2019/src/main/java/com/atguigu/interview/study/thread/CyclicBarrierDemo.java
@@ -1,5 +1,6 @@
 package com.atguigu.interview.study.thread;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 /**
@@ -8,13 +9,20 @@ import java.util.concurrent.CyclicBarrier;
  */
 public class CyclicBarrierDemo {
     public static void main(String[] args) {
-        new CyclicBarrier(7, () -> {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> {
             System.out.println("**************召唤神龙");
         });
         for (int i = 0; i < 10; i++) {
             final int tempInt = i;
             new Thread(() -> {
                 System.out.println(Thread.currentThread().getName() + "\t收集到第：" + tempInt + "龙珠");
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
             }, String.valueOf(i)).start();
         }
 
