77ca83bdb81373804520ea8ddac57d5c127db493
lixixi89055465 <nanji1850094299@outlook.com>
2023-03-05 10:30:23 +0800

    init

---
+++ jvm_stu/src/main/java/com/atguigu/juc1205/ThreadWaitNotifyDemo.java
@@ -0,0 +1,81 @@
+package com.atguigu.juc1205;
+
+class AirConditioner {//资源类
+    private int number = 0;
+
+    public synchronized void increment() throws InterruptedException {
+        //1 判断
+        while (number != 0) {
+            this.wait();
+        }
+        //2 干活
+        number++;
+        System.out.println(Thread.currentThread().getName() + "\t" + number);
+        //3 通知
+        this.notifyAll();
+    }
+
+    public synchronized void decrement() throws InterruptedException {
+        //1 判断
+        while (number == 0) {
+            this.wait();
+        }
+        //2 干活
+        number--;
+        System.out.println(Thread.currentThread().getName() + "\t" + number);
+        //3 通知
+        this.notifyAll();
+    }
+
+
+}
+
+/**
+ * 题目:现在两个线程，可以操作初始值为零的一个变量，
+ * 实现一个线程对该变量加1，一个线程对该变量减一，
+ * 实现交替，来10轮，变量初始值为零。
+ * 1    高内聚低耦合前提下，线程操作资源类
+ * 2    判断/干活/通知
+ */
+public class ThreadWaitNotifyDemo {
+    public static void main(String[] args) {
+        AirConditioner airConditioner = new AirConditioner();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    airConditioner.increment();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "AAA").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    airConditioner.decrement();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "BBB").start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    airConditioner.increment();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "CCC").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    airConditioner.decrement();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "DDD").start();
+    }
+}
