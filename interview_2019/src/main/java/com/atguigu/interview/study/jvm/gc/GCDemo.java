f23347b229f486d4ce6a58fecc71c2d718f9743f
lixixi89055465 <nanji1850094299@outlook.com>
2023-03-09 11:35:55 +0800

    OOM异常

--- interview_2019/src/main/java/com/atguigu/interview/study/jvm/gc/GCDemo.java
+++ interview_2019/src/main/java/com/atguigu/interview/study/jvm/gc/GCDemo.java
@@ -5,21 +5,45 @@ import java.util.Random;
 /**
  * @author lixiang
  * @date 2020-05-22-16:58
+ * 1
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseSerialGC  (DefNew + Tenured)
+ * 2
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParNewGC  (ParNew + Tenured)
+ * Java HotSpot(TM) 64-Bit Server VM warning:
+ * Using the ParNew young collector with the Serial old collector is deprecated and will likely
+ * be removed in a future release
+ *
+ * 3
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParalleGC  (PSYoungGen + ParoldGen)
+ *
+ * 4
+ * 4.1
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandFlags -XX:+UseParallelOldGC (PSYoungGen + ParoldGen)
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:UseG1GC
  */
 public class GCDemo {
     public static void main(String[] args) {
         System.out.println("************GCDemo hello");
 
-        try {
-            String str = "atguigu";
-            while (true) {
-                str += str + new Random().nextInt(7777777) + new Random().nextInt(88888888);
-                str.intern();
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+//        try {
+//            String str = "atguigu";
+//            while (true) {
+//                str += str + new Random().nextInt(7777777) + new Random().nextInt(88888888);
+//                str.intern();
+//            }
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
 
     }
 }
